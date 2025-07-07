170986e9110f8c58f47af0f85028a794c6a11f64
OJPARKINSON <olliepar01@gmail.com>
2025-07-07 23:11:07 +0100

    testing func 2

--- Program.cs
+++ Program.cs
@@ -19,6 +19,13 @@ using (var csv = new CsvHelper.CsvReader(reader, CultureInfo.InvariantCulture))
         // double func1 = funcs.Function1(double.Parse(raceParams["base_grip"].Value), double.Parse(raceParams["tire_wear_rate"].Value), csv.GetField<int>(1) - 1, double.Parse(raceParams["degradation_factor"].Value), double.Parse(raceParams["reference_lap_time"].Value), double.Parse(raceParams["grip_coefficient"].Value));
         // Console.WriteLine(func1);
 
+        if (csv.GetField<double>(0) == 0.0)
+        {
+            double func2 = funcs.Function2(double.Parse(raceParams["base_consumption"].Value), double.Parse(raceParams["weight_penalty"].Value), double.Parse(raceParams["current_fuel"].Value), csv.GetField<int>(1) - 1);
+            Console.WriteLine(func2);
+        }
+
+
     }
 }
 ;
